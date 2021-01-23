e1a779bf43993b764e21e28916f3b6c7c257713c
jovialhsu <celia0528@gmail.com>
2021-01-23 19:22:16 +0800

    upd

--- config/uri/client.js
+++ config/uri/client.js
@@ -3,23 +3,23 @@
  */
 //apiUri
 exports.mainApi = {
-    header: '/api/header',
-    footer: '/api/footer',
-    EzJsCss: '/api/EzJsCss',
+  header: "/join/api/header",
+  footer: "/api/footer",
+  EzJsCss: "/api/EzJsCss",
 };
 exports.b2e = {
-    member: '/api/b2eMember',
+  member: "/api/b2eMember",
 };
 
 //Endpoint & staticUrl
 const member = (env) => {
-    switch (env) {
-        case 'PRD':
-            return 'https://member.eztravel.com.tw/';
-        case 'WS':
-            return 'https://mem-ws01.eztravel.com.tw/';
-        case 'TEST':
-        default:
-            return '//mem00t-w01.eztravel.com.tw/';
-    }
+  switch (env) {
+    case "PRD":
+      return "https://member.eztravel.com.tw/";
+    case "WS":
+      return "https://mem-ws01.eztravel.com.tw/";
+    case "TEST":
+    default:
+      return "//mem00t-w01.eztravel.com.tw/";
+  }
 };
